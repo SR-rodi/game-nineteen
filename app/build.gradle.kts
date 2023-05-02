ceb19512f50f52882e772637f3edd58cb64a9ebc
Sergey Rodin <rodin_sr@mail.ru>
2023-05-02 11:10:45 +0300

    gradleKts

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("kotlin-parcelize")
+    id ("androidx.navigation.safeargs.kotlin")
+    id ("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        namespace = "com.example.nineteen_2_0"
+        applicationId = "com.example.nineteen_2_0"
+        minSdk = 29
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            //proguardFiles getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+        }
+    }
+    compileOptions   {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility  = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding  = true
+    }
+
+}
+
+dependencies {
+
+    implementation ("com.google.code.gson:gson:2.9.0")
+
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-android-compiler:2.44")
+
+    implementation ("androidx.room:room-runtime:2.4.3")
+    implementation ("androidx.room:room-ktx:2.4.3")
+    kapt ("androidx.room:room-compiler:2.4.3")
+
+
+    implementation ("io.insert-koin:koin-android:3.4.0")
+
+    implementation( "androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation ("androidx.fragment:fragment-ktx:1.5.3")
+
+    implementation ("androidx.core:core-ktx:1.9.0")
+    implementation ("androidx.appcompat:appcompat:1.5.1")
+    implementation ("com.google.android.material:material:1.6.1")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+}
