73e8cb3f24b7cf2337e5ee6b559aa8cd8e716105
Sergey Rodin <rodin_sr@mail.ru>
2023-05-10 23:10:50 +0300

    Настроил анимацию для итема помошника

--- feature_game/src/main/kotlin/ru/sr/nineteen/presentation/field/compose/view/GameItemView.kt
+++ feature_game/src/main/kotlin/ru/sr/nineteen/presentation/field/compose/view/GameItemView.kt
@@ -1,5 +1,11 @@
 package ru.sr.nineteen.presentation.field.compose.view
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,8 +14,11 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -21,15 +30,23 @@ fun BaseGameItem(
     item: GameItemEngine,
     cardColor: Color = GameTheme.colors.notChoice,
     borderColor: Color? = null,
+    rotate: Float = 0F,
+    scale: Float = 1F,
     onClickItem: () -> Unit = {},
 ) {
     val modifier =
-        if (borderColor == null) Modifier else Modifier.border(width = 2.dp, color = borderColor)
+        if (borderColor == null) Modifier else Modifier.border(
+            width = 2.dp,
+            color = borderColor,
+            shape = GameTheme.shapes.small
+        )
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .clip(shape = GameTheme.shapes.small)
             .padding(4.dp)
+            .rotate(rotate)
+            .scale(scale)
             .then(modifier)
             .clickable { onClickItem() },
         shape = GameTheme.shapes.small,
@@ -83,10 +100,32 @@ fun GameItemHelpView(
     onClickItem: () -> Unit,
 ) {
 
+    val transition = rememberInfiniteTransition()
+    val rotate by transition.animateFloat(
+        initialValue = -5f,
+        targetValue = 5f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(500,easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val scale by transition.animateFloat(
+        initialValue = 1F,
+        targetValue = 0.9f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+
     BaseGameItem(
         item = item,
         cardColor = cardColor,
         onClickItem = onClickItem,
-        borderColor = borderColor
+        borderColor = borderColor,
+        rotate = rotate,
+        scale = scale
     )
 }
